9061cfe323341e16bc298db2eca156911d06aef6
icyzeroice <ziv3@outlook.com>
2017-10-18 16:29:31 +0800

    RESTART

--- demo/dom.js
+++ demo/dom.js
@@ -1,17 +1,3 @@
 AMD.module('./dom', function () {
-  return {
-    // getElementById
-    g: function (id) {
-      console.log(document.getElementById(id));
-      return document.getElementById(id);
-    },
-    // insert html in element id
-    html: function (id, html) {
-      if (html) {
-        this.g(id).innerHTML = html;
-      } else {
-        return this.g(id).innerHTML;
-      }
-    }
-  };
+  return console.log('this is dom module');
 });
--- demo/event.js
+++ demo/event.js
@@ -1,9 +1,3 @@
 AMD.module('./event', ['./dom'], function (dom) {
-  let events = {
-    // bind ['type'] event to element id with function fn
-    on: function (id, type, fn) {
-      dom.g(id)['on' + type] = fn;
-    }
-  }
-  return events;
+  return console.log('this is event module', dom);
 });
